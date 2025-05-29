9e4703e7913baa432957202fdea142099e5e9271
Dmitrijs Savkins <dmitrijs.savkins@gmail.com>
2025-05-29 18:03:48 +0300

    Make home row mods toggleable

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/dhmitry/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/dhmitry/keymap.c
@@ -30,6 +30,7 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum custom_layers {
     _BL,
+    _HM,
     _FL,
 };
 
@@ -38,10 +39,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_BL] = LAYOUT_65_ansi_blocker(
   QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
-  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
-  KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,             KC_PGDN,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  TG(_HM),
+  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             TG(_HM),
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-  KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT
+  KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_FL),  KC_LEFT,  KC_DOWN,  KC_RGHT
+  ),
+
+  /* Keymap _HM: Home row mods layer
+   */
+[_HM] = LAYOUT_65_ansi_blocker(
+  QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  TG(_HM),
+  KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,             TG(_HM),
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
+  KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(_FL),  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   /* Keymap _FL: Function Layer
@@ -54,3 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  _______,  _______,                                QK_BOOT,                                _______,  _______,  RM_PREV,   RM_VALD,  RM_NEXT
   )
 };
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case _HM:
+            rgb_matrix_mode(RGB_MATRIX_JELLYBEAN_RAINDROPS);
+            break;
+        case _FL:
+            rgb_matrix_mode(RGB_MATRIX_CYCLE_OUT_IN);
+            break;
+        default:
+            rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+            break;
+    }
+    return state;
+}
