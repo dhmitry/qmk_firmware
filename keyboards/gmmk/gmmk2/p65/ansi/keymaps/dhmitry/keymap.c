d9b794d43048864c7ed2df3205b7e5e3681fc2fb
Dmitrijs Savkins <dmitrijs.savkins@gmail.com>
2025-05-29 17:24:10 +0300

    Add homerow mods

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/dhmitry/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/dhmitry/keymap.c
@@ -16,6 +16,16 @@
 
 #include QMK_KEYBOARD_H
 
+#define HOME_A    LGUI_T(KC_A)
+#define HOME_S    LALT_T(KC_S)
+#define HOME_D    LCTL_T(KC_D)
+#define HOME_F    LSFT_T(KC_F)
+
+#define HOME_J    RSFT_T(KC_J)
+#define HOME_K    RCTL_T(KC_K)
+#define HOME_L    RALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum custom_layers {
@@ -29,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = LAYOUT_65_ansi_blocker(
   QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
-  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_PGDN,
+  KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,             KC_PGDN,
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
